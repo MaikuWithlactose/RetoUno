924bd072e2990f2794cf333512ae71bec1eeb366
MaikaNunez <Maikanunezolmos@gmail.com>
2023-05-16 23:23:08 +0200

    correctivo

--- reto2/index.js
+++ reto2/index.js
@@ -1,6 +1,6 @@
-function suma(a, b) {
-    return a + b;
-}
+// function suma(a, b) {
+//     return a + b;
+// }
 
 function resta(a, b) 
 {
@@ -24,6 +24,6 @@ function cuadrado(a)
 {
     return Math.pow(a,2);
 }
-
+git push [-u | --set-upstream] <remote> <branch></branch>
 module.exports = { suma, resta, producto, division , cuadrado};
   
