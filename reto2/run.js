8da326b6e594ee1b219a6ca2da94be552d0f291a
MaikaNunez <Maikanunezolmos@gmail.com>
2023-05-16 23:24:04 +0200

    Cambios rama2

--- reto2/run.js
+++ reto2/run.js
@@ -1,6 +1,6 @@
 let myLib = require("./index.js");
 
-console.log("SUMA : " + myLib.suma(2, 3)); 
+console.log("SUMA : " + myLib.suma(2, 3, 4)); 
 console.log("RESTA : " + myLib.resta(5, 3)); 
 console.log("PRODUCTO/MULTIPLICACION : " + myLib.producto(4, 6)); 
 console.log("DIVISION : " + myLib.division(10, 2)); 
